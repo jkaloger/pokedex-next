b45fecafd897af882f005ec2cf04c88f2de7dd48
Jack Kaloger <jkaloger@users.noreply.github.com>
2024-06-01 22:11:01 +1000

    flags

---
+++ app/.well-known/vercel/flags/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { verifyAccess, type ApiData } from '@vercel/flags';
+import { merge } from 'lodash';
+import Flagsmith from 'flagsmith-nodejs';
+
+export async function GET(request: NextRequest) {
+	const access = await verifyAccess(request.headers.get('Authorization'));
+	if (!access) return NextResponse.json(null, { status: 401 });
+
+	const flagsmith = new Flagsmith({
+		environmentKey: process.env.FLAGSMITH_SSR_KEY,
+	});
+
+	const flags = await flagsmith.getEnvironmentFlags();
+	const definitions = flags.allFlags().map((flag) => ({
+		[flag.featureName]: {
+			options: [
+				{ value: false, label: 'Off' },
+				{ value: true, label: 'On' },
+			],
+		},
+	}));
+
+	return NextResponse.json<ApiData>({
+		definitions: merge({}, ...definitions),
+	});
+}
