d3c8df5efef800a5f2116127190723b65b088711
Jack Kaloger <jkaloger@users.noreply.github.com>
2024-06-01 20:46:07 +1000

    env vars

--- lib/api.ts
+++ lib/api.ts
@@ -1,25 +1,25 @@
 import type { PokeAPI } from 'pokeapi-types';
 
 export const getPokemon = async (offset?: number, limit?: number): Promise<PokeAPI.NamedAPIResourceList> => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset ?? 0}&limit=${limit ?? 10}`;
+  const url = `${process.env.POKEAPI_URL}/pokemon?offset=${offset ?? 0}&limit=${limit ?? 10}`;
   const response = await fetch(url, { cache: 'force-cache' });
   return await response.json();
 };
 
 export const getPokemonById = async (id: string): Promise<PokeAPI.Pokemon> => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const url = `${process.env.POKEAPI_URL}/pokemon/${id}`;
   const response = await fetch(url, { cache: 'force-cache' });
   return await response.json();
 };
 
 export const getBerryById = async (id: string): Promise<PokeAPI.Berry> => {
-  const url = `https://pokeapi.co/api/v2/berry/${id}`;
+  const url = `${process.env.POKEAPI_URL}/berry/${id}`;
   const response = await fetch(url, { cache: 'force-cache' });
   return await response.json();
 };
 
 export const getItemById = async (id: string): Promise<PokeAPI.Item> => {
-  const url = `https://pokeapi.co/api/v2/item/${id}`;
+  const url = `${process.env.POKEAPI_URL}/item/${id}`;
   const response = await fetch(url, { cache: 'force-cache' });
   return await response.json();
 };
