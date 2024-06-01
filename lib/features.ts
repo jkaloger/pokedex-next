0588022c72ecd6c64ae7b5e5d289478804d37f61
Jack Kaloger <jkaloger@users.noreply.github.com>
2024-06-01 22:16:52 +1000

    logging

--- lib/features.ts
+++ lib/features.ts
@@ -1,18 +1,4 @@
 import Flagsmith from 'flagsmith-nodejs';
-import { FlagOverridesType, decrypt } from '@vercel/flags';
-import { type NextRequest } from 'next/server';
-import { cookies } from 'next/headers';
-
-export async function getFlags(request: NextRequest) {
-  const overrideCookie = cookies().get('vercel-flag-overrides')?.value;
-  const overrides = overrideCookie ? await decrypt<FlagOverridesType>(overrideCookie) : {};
-
-  const flags = {
-    exampleFlag: overrides?.exampleFlag ?? false,
-  };
-
-  return flags;
-}
 
 export const flagsmith = new Flagsmith({
   environmentKey: process.env.FLAGSMITH_SSR_KEY,
